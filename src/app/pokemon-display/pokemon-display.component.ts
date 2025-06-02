faccc5609590048ec81a23faeeb80a4ce2ca8f71
Rubiksel <ejderaksel@gmail.com>
2025-06-02 16:19:53 -0500

    chore: start building routes

---
+++ src/app/pokemon-display/pokemon-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-display',
+  imports: [],
+  templateUrl: './pokemon-display.component.html',
+  styleUrl: './pokemon-display.component.css'
+})
+export class PokemonDisplayComponent {
+
+}
