c7c9cecfd4907cb4c7b535ca806b01177632f573
Rubiksel <ejderaksel@gmail.com>
2025-06-04 16:26:45 -0500

    feat: create landing page and display page UIs

--- src/app/pokemon-display/pokemon-display.component.ts
+++ src/app/pokemon-display/pokemon-display.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { PokemonGridComponent } from '../pokemon-grid/pokemon-grid.component';
 
 @Component({
   selector: 'app-pokemon-display',
-  imports: [],
+  imports: [HeaderComponent, PokemonGridComponent],
   templateUrl: './pokemon-display.component.html',
-  styleUrl: './pokemon-display.component.css'
+  styleUrl: './pokemon-display.component.css',
 })
-export class PokemonDisplayComponent {
-
-}
+export class PokemonDisplayComponent {}
