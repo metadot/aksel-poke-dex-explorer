40f3d4d0550f12990a36527a71a53249d0855254
Rubiksel <76902702+Rubiksel@users.noreply.github.com>
2025-06-06 12:29:42 -0500

    feat: added display component and navigation (#3)
    * set up angular project in right directory
    
    * corrected angular doc link
    
    * chore: start building routes
    
    * feat: start landing page build with pokeball and play button
    
    * feat: create landing page and display page UIs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PokemonDisplayComponent } from './pokemon-display/pokemon-display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'display', component: PokemonDisplayComponent },
+];
---
+++ src/app/pokemon-grid/pokemon-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-grid',
+  imports: [],
+  templateUrl: './pokemon-grid.component.html',
+  styleUrl: './pokemon-grid.component.css'
+})
+export class PokemonGridComponent {
+
+}
