005af73671fce8c3dbc1283b2547d0ed71ada6b1
Rubiksel <ejderaksel@gmail.com>
2025-06-16 14:13:04 -0500

    refactor: remove all async calls and use httpResource and signals

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, httpResource } from '@angular/common/http';
-import { Component, effect, input } from '@angular/core';
+import { Component, computed, effect, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { first, firstValueFrom } from 'rxjs';
+import { combineLatest, first, firstValueFrom } from 'rxjs';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
@@ -12,77 +12,24 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
   styleUrl: './pokemon-list.component.css',
 })
 export class PokemonListComponent {
-  pokemons: any[] = [];
-  types: any[] = [];
+  readonly page = input<number>(0);
+  readonly limit = input<number>(12);
 
-  readonly page = input.required<number>();
-  readonly limit = input.required<number>();
+  readonly offset = computed(() => this.page() * this.limit());
 
-  constructor(private http: HttpClient, private router: Router) {
+  readonly pokemons = httpResource<any>(() => ({
+    url: `https://pokeapi.co/api/v2/pokemon-species`,
+    params: {
+      limit: this.limit(),
+      offset: this.offset(),
+    },
+  }));
+
+  constructor(private router: Router) {
     effect(() => {
       console.log(`This is the page ${this.page()}`);
       console.log(`This is the number of items per page ${this.limit()}`);
-      this.getPokemonInfo();
     });
-  }
-
-  getTypes() {
-    return this.http.get(`https://pokeapi.co/api/v2/type`);
-  }
-
-  getTypeData(url: string) {
-    return this.http.get(`${url}`);
-  }
-
-  getPokemonSpecies(offset: number) {
-    return this.http.get(
-      `https://pokeapi.co/api/v2/pokemon-species?limit=${this.limit()}&offset=${offset}`
-    );
-  }
-
-  async getPokemonInfo() {
-    const offset = this.limit() * this.page();
-    const typeResponse: any = await firstValueFrom(this.getTypes());
-    const typePromises = typeResponse.results.map((result: any) =>
-      firstValueFrom(this.getTypeData(result.url))
-    );
-
-    const speciesResponse: any = await firstValueFrom(
-      this.getPokemonSpecies(offset)
-    );
-
-    const pokemonPromises = speciesResponse.results.map(
-      async (species: any) => {
-        const speciesData: any = await firstValueFrom(
-          this.getPokemonData(species.url)
-        );
-        const detailedPokemon = await firstValueFrom(
-          this.http.get(speciesData.varieties[0].pokemon.url)
-        );
-        return {
-          species_name: species.name,
-          ...detailedPokemon,
-        };
-      }
-    );
-    this.types = await Promise.all(typePromises);
-    this.pokemons = await Promise.all(pokemonPromises);
-  }
-
-  getPokemonData(url: string) {
-    return this.http.get(url);
-  }
-
-  getTypeId(typeName: string) {
-    const match = this.types.find((t) => t.name === typeName);
-    return match ? match.id : null;
-  }
-
-  getTypeSprite(typeName: string) {
-    const typeId = this.getTypeId(typeName);
-
-    const type = this.types[typeId - 1];
-    return type.sprites['generation-ix']['scarlet-violet']['name_icon'];
   }
 
   async handlePageEvent(pageEvent: PageEvent) {
