b404c2c63ef34a28fc999568a311f9fa7b6c5281
Rubiksel <ejderaksel@gmail.com>
2025-06-17 11:39:30 -0500

    refactor: switch from css to bootstrap refactor: add file for global constants refactor: rename and remove unnecessary code for onPageChanged function

---
+++ src/app/_core/constants/pagination.constants.ts
@@ -0,0 +1,4 @@
+export const limitParam = 12;
+export const pageParam = 0;
+export const pokemonNumber = 1025;
+export const pageSizeOptions = [12, 24, 48, 72];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { limitParam, pageParam } from '../_core/constants/pagination.constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  limitParam = limitParam;
+  pageParam = pageParam;
+}
