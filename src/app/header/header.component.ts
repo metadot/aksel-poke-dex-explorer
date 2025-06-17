c8862069785be166ca58b49944ee1c5fe54f5e3b
Rubiksel <ejderaksel@gmail.com>
2025-06-17 14:10:23 -0500

    refactor: use bootstrap feat: implement search

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { limitParam, pageParam } from '../_core/constants/pagination.constants';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
