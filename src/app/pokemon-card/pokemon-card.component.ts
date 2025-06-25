8ed277a2e9f600459e5ea6b18ee4c80af59642ec
Rubiksel <ejderaksel@gmail.com>
2025-06-25 15:24:11 -0500

    refactor: replace al l httpResource inputs in pokemon card component with Pokemon and PokemonSpecies signals refactor: use angular built-in form system fix: reorganize pokemon detailed view component

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -3,22 +3,22 @@ import { Component, effect, input, output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Pokemon, PokemonSpecies } from '../_core/models/pokemon';
 import { HttpResourceRef } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-card',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './pokemon-card.component.html',
 })
 export class PokemonCardComponent {
-  readonly pokemon = input.required<HttpResourceRef<Pokemon | undefined>>();
-  readonly basePokemon = input.required<HttpResourceRef<Pokemon | undefined>>();
-  readonly pokemonSpecies =
-    input.required<HttpResourceRef<PokemonSpecies | undefined>>();
-  readonly previousSpecies =
-    input<HttpResourceRef<PokemonSpecies | undefined>>();
-  readonly previousPokemon = input<HttpResourceRef<Pokemon | undefined>>();
-  readonly nextSpecies = input<HttpResourceRef<PokemonSpecies | undefined>>();
-  readonly nextPokemon = input<HttpResourceRef<Pokemon | undefined>>();
+  readonly pokemon = input.required<Pokemon | undefined>();
+  readonly basePokemon = input.required<Pokemon | undefined>();
+  readonly pokemonSpecies = input.required<PokemonSpecies | undefined>();
+  readonly speciesError = input.required<unknown>();
+  readonly previousSpecies = input<PokemonSpecies | undefined>();
+  readonly previousPokemon = input<Pokemon | undefined>();
+  readonly nextSpecies = input<PokemonSpecies | undefined>();
+  readonly nextPokemon = input<Pokemon | undefined>();
   readonly flavorText = input<string>();
   readonly latestCry = input<string>();
   readonly legacyCry = input<string>();
@@ -28,8 +28,7 @@ export class PokemonCardComponent {
   selectedForm = input.required<string | undefined>();
   formSelected = output<string>();
 
-  onFormChange(event: Event) {
-    const value = (event.target as HTMLSelectElement).value;
+  onFormChange(value: string) {
     this.formSelected.emit(value);
   }
 }
