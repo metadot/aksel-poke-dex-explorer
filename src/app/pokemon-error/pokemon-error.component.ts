719190d658f2515a7e228155c5fac00baee91395
Rubiksel <ejderaksel@gmail.com>
2025-06-26 10:11:55 -0500

    refactor: make pokemon detailed component handle errors and loading refactor: create component to display error refactor: add required property back to inputs that need it

---
+++ src/app/pokemon-error/pokemon-error.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-error',
+  imports: [],
+  templateUrl: './pokemon-error.component.html',
+})
+export class PokemonErrorComponent {}
