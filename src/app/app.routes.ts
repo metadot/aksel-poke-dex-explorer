3c07c94a48161a176a304d698afe58f3d5a0a25d
Rubiksel <ejderaksel@gmail.com>
2025-07-01 12:43:40 -0500

    misc: add comments to format pokemon name pipe feat: add safeguard to access pokemon list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes, withComponentInputBinding } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailedViewComponent } from './pokemon-detailed-view/pokemon-detailed-view.component';
+import { listPasswordGuard } from './list-password.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pokemons', component: PokemonListComponent },
+  {
+    path: 'pokemons',
+    component: PokemonListComponent,
+    canActivate: [listPasswordGuard],
+  },
   {
     path: 'pokemons/:name',
     component: PokemonDetailedViewComponent,
